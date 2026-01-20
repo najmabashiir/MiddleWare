190e547ee8910d4fdd13eb7c6924fc57e786bd75
najmabashiir <najaxbashiirali>
2026-01-20 16:28:40 +0300

    // Built-in Middleware: express.static()
    Signed-off-by: najmabashiir <najaxbashiirali>

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json()); // Built-in middleware to parse JSON bodies
+// app.use(express.json()); // Built-in middleware to parse JSON bodies
 
-app.post('/data', (req, res) => {
-    res.json({received: req.body});
-});
+// app.post('/data', (req, res) => {
+//     res.json({received: req.body});
+// });
+
+// Built-in Middleware: express.static()
+app.use(express.static('public')); // Serve static files from 'public' directory
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
+
+
+
